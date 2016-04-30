ddaabc80ebce334d4eb5dc0c8c9e6b7afdcd5c90
sandoh <andoh.samuel@gmail.com>
2016-04-29 22:47:25 -0500

    I just added the third test

--- src/test/java/pageFactoryTest/LoginPageTest.java
+++ src/test/java/pageFactoryTest/LoginPageTest.java
@@ -53,10 +53,28 @@ public class LoginPageTest {
 		
 		@Test
 		public void bingSearchTest1() throws InterruptedException{
+	     // invoke the beforeTest method over here 
 			setup();
 		LoginPage bingSearch = new LoginPage(driver);
 		bingSearch.searchBing("Books");
 	     //String searchFind = "driver.getPageSource().contains(Becker Books)";
+	     boolean test = driver.getPageSource().contains("Online shopping");
+		
+		Assert.assertEquals(test, true);
+	  
+	     driver.close();
+	     
+	     Thread.sleep(2000);
+		}
+		@Test
+		public void bingSearchTest2() throws InterruptedException{
+	     // invoke the beforeTest method over here 
+			setup();
+			
+		LoginPage bingSearch = new LoginPage(driver);
+		bingSearch.searchBing("Books");
+	     //String searchFind = "driver.getPageSource().contains(Becker Books)";
+		System.out.println(" verifying the test contains online shopping 3 ");
 	     boolean test = driver.getPageSource().contains("Online shopping");
 		
 		Assert.assertEquals(test, true);
