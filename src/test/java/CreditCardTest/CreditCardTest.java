8c71937b675f9071a0b37c321bdaa12b0c9ec74c
sandoh <andoh.samuel@gmail.com>
2016-09-22 12:35:46 -0500

    release 2

--- src/test/java/CreditCardTest/CreditCardTest.java
+++ src/test/java/CreditCardTest/CreditCardTest.java
@@ -1,5 +1,39 @@
 package CreditCardTest;
+/**
+ * 
+ */
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+
+import login.login;
 
 public class CreditCardTest {
 
+	WebDriver driver = new HtmlUnitDriver();
+		
+	@Test
+	
+  public void app_Test() throws InterruptedException{
+		
+	login Ltest=PageFactory.initElements(driver, login.class);
+	  
+	Ltest.app_login();
+	Thread.sleep(5000);
+	boolean app_login_books= driver.getPageSource().contains("login");
+	Assert.assertEquals(app_login_books, true);
+  }
+  
+	@Test
+	 public void app_gmailAccountTest() throws Throwable{
+	login gmailObject=PageFactory.initElements(driver, login.class);
+	gmailObject.app_gmailAccount("sammy27gh@yahoo.com", "ska123456");
+	boolean app_gmailAccountValidate= driver.getPageSource().contains("tag");
+	Assert.assertEquals(app_gmailAccountValidate, true);
+	 } 
 }
